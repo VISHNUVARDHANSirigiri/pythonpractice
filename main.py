53971c9fd23599aef31bd1a4e4288e9c5d4287e9
VISHNUVARDHANSirigiri <65660880+VISHNUVARDHANSirigiri@users.noreply.github.com>
2024-09-27 13:09:42 -0500

    Initial files

---
+++ main.py
@@ -0,0 +1,11 @@
+from substracy import substract
+
+
+def add(a,b):
+    print(a+b)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    add(3,7)
+    substract(10,11)
